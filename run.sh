7ffc0ea2fc42d448415a486b576b0ccca9c1bf64
Carlo Mandelli <camandel@gmail.com>
2018-02-26 15:41:23 +0100

    Added new parameter
    Added CNTLM_AUTH parameter and modified checks

--- run.sh
+++ run.sh
@@ -1,45 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ -z "${CNTLM_PROXY}" ]
+then
+        echo "CNTLM_PROXY can't be empty"
+        exit 1
+fi
 
 cat << EOF > /cntlm.conf
 Proxy       ${CNTLM_PROXY}
-
 NoProxy     ${CNTLM_NO_PROXY}
-
+Auth        ${CNTLM_AUTH}
 Gateway     yes
 Listen      0.0.0.0:3128
-
 EOF
 
-if [ ! -z "${CNTLM_USERNAME}" ]
-then
-echo "Username    ${CNTLM_USERNAME}" >> /cntlm.conf
-fi
-
-if [ ! -z "${CNTLM_DOMAIN}" ]
-then
-echo "Domain      ${CNTLM_DOMAIN}" >> /cntlm.conf
-fi
-
-if [ ! -z "${CNTLM_PASSWORD}" ]
-then
-echo "Password      ${CNTLM_PASSWORD}" >> /cntlm.conf
-fi
-
-if [ ! -z "${CNTLM_PASSNTLMV2}" ]
-then
-echo "PassNTLMv2      ${CNTLM_PASSNTLMV2}" >> /cntlm.conf
-fi
-
-if [ ! -z "${CNTLM_PASSNT}" ]
-then
-echo "PassNT      ${CNTLM_PASSNT}" >> /cntlm.conf
-fi
-
-if [ ! -z "${CNTLM_PASSLM}" ]
-then
-echo "PassLM      ${CNTLM_PASSLM}" >> /cntlm.conf
-fi
+[ -n "${CNTLM_USERNAME}" ] && echo "Username    ${CNTLM_USERNAME}" >> /cntlm.conf
+[ -n "${CNTLM_DOMAIN}" ] && echo "Domain      ${CNTLM_DOMAIN}" >> /cntlm.conf
+[ -n "${CNTLM_PASSWORD}" ] && echo "Password    ${CNTLM_PASSWORD}" >> /cntlm.conf
+[ -n "${CNTLM_PASSNTLMV2}" ] && echo "PassNTLMv2      ${CNTLM_PASSNTLMV2}" >> /cntlm.conf
+[ -n "${CNTLM_PASSNT}" ] && echo "PassNT      ${CNTLM_PASSNT}" >> /cntlm.conf
+[ -n "${CNTLM_PASSLM}" ] && echo "PassLM      ${CNTLM_PASSLM}" >> /cntlm.conf
 
 cntlm -f -v -c /cntlm.conf $*
-
-
